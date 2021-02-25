b48922d048d42ca03d60843dc86824fefdc74093
Abdullah Ugraskan <apostylee@apo.local>
2021-02-25 23:35:38 +0300

    dev

---
+++ src/components/MyButton/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <button>{{ text }}</button>
+</template>
+
+<script>
+export default {
+  props: {
+    text: {
+      type: String,
+      default: "submit",
+    },
+  },
+};
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,87 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <data-table :schema="schema" :items="items"> </data-table>
+    <!-- 
+    <hr />
+
+    <data-table :schema="schema2" :items="items" /> -->
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import DataTable from "@/components/DataTable";
 
 export default {
-  name: 'Home',
+  name: "Home",
   components: {
-    HelloWorld
-  }
-}
+    DataTable,
+  },
+
+  data() {
+    return {
+      schema: {
+        showSearch: true,
+        searchField: "name",
+        showFooter: true,
+        fields: [
+          {
+            field: "id",
+            title: "Id",
+            width: "20px",
+          },
+          {
+            field: "name",
+            title: "Adı",
+          },
+          {
+            field: "city",
+            title: "Şehir",
+          },
+        ],
+      },
+      items: [
+        {
+          id: 1,
+          name: "Tufan",
+          city: "Ankara",
+        },
+        {
+          id: 12,
+          name: "Taramalı Tüfek",
+          city: "Ankara",
+        },
+        {
+          id: 2,
+          name: "Ömer",
+          city: "Trabzon",
+        },
+        {
+          id: 3,
+          name: "Nil",
+          city: "İstanbul",
+        },
+      ],
+
+      schema2: {
+        showSearch: true,
+        searchField: "name",
+        fields: [
+          {
+            field: "id",
+            title: "Id",
+            width: "20px",
+          },
+          {
+            field: "name",
+            title: "Adı",
+          },
+          {
+            field: "city",
+            title: "Şehir",
+          },
+        ],
+      },
+    };
+  },
+};
 </script>
